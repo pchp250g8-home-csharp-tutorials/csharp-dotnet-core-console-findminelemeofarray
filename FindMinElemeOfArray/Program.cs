4c768f623e5e07756534702c8d65b883b1ae0c29
Alexander <pchp250g8@tutamail.com>
2026-01-08 14:04:03 +0300

    Добавьте файлы проекта.

---
+++ FindMinElemeOfArray/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+var oRnd = new Random();
+Console.WriteLine("Input a count of elements:");
+int.TryParse(Console.ReadLine(), out var nElems);
+if (nElems == 0)
+{
+    Console.WriteLine("Empty arrays not allowed");
+    Console.Read();
+    return;
+}
+Console.WriteLine($"Generating array by filling {nElems}:");
+int[] iNumbers = new int[nElems];
+for (int i = 0; i < nElems; i++)
+{
+    iNumbers[i] = oRnd.Next(1, 100);
+    Console.Write(iNumbers[i] + " ");
+}
+var iMin = iNumbers[0];
+var nIndex = 0;
+for (int i = 1; i < nElems; i++)
+{
+    if (iNumbers[i] < iMin)
+    {
+        iMin = iNumbers[i];
+        nIndex = i;
+    }
+}
+Console.WriteLine($"\r\nFound minimum element:{iMin} with index {nIndex}");
+Console.Read();
